bf2c3bdf67e7073555eda3f7e2d7a4cfb0a908a3
Parth Shandilya <parth.shandilya@cern.ch>
2022-07-21 16:38:45 +0200

    add encryption and decryption of data

--- export-services.py
+++ export-services.py
@@ -1,49 +1,107 @@
 #!/usr/bin/env python
 
+import base64
 import json
 import logging
 import os
 
 import boto3
 import requests
+from cryptography.fernet import Fernet
+
+NUM_BYTES_FOR_LEN = 4
+
+
+def create_cmk(description):
+    """Creates a KMS Customer Master Key
+
+    Description is used to differentiate between CMKs.
+    """
+
+    kms_client = boto3.client("kms")
+    response = kms_client.create_key(Description=description)
+
+    # Return the key ID and ARN
+    return response["KeyMetadata"]["KeyId"], response["KeyMetadata"]["Arn"]
+
+
+def create_data_key(cmk_id, key_spec="AES_256"):
+    """Generate a data key to use when encrypting and decrypting data"""
+
+    # Create data key
+    kms_client = boto3.client("kms")
+    response = kms_client.generate_data_key(KeyId=cmk_id, KeySpec=key_spec)
+
+    # Return the encrypted and plaintext data key
+    return response["CiphertextBlob"], base64.b64encode(response["Plaintext"])
+
+
+def encrypt_file(filename, cmk_id):
+    """Encrypt JSON data using an AWS KMS CMK"""
+    with open(filename, "rb") as file:
+        file_contents = file.read()
+
+    data_key_encrypted, data_key_plaintext = create_data_key(cmk_id)
+    if data_key_encrypted is None:
+        logging.error("Encryption key not available.")
+
+    # Encrypt the data
+    f = Fernet(data_key_plaintext)
+    file_contents_encrypted = f.encrypt(file_contents)
+
+    # Write the encrypted data key and encrypted file contents together
+    with open(filename, "wb") as file_encrypted:
+        file_encrypted.write(
+            len(data_key_encrypted).to_bytes(NUM_BYTES_FOR_LEN, byteorder="big")
+        )
+        file_encrypted.write(data_key_encrypted)
+        file_encrypted.write(file_contents_encrypted)
+    logging.info("File encrypted successfully.")
+
+    return
 
 
 def export_services():
-    file_name, bucket_name, access_key, secret_key, s3_host, host_url, token = (
-        os.environ["FILE_NAME"],
-        os.environ["BUCKET_NAME"],
-        os.environ["S3_ACCESS_KEY"],
-        os.environ["S3_SECRET_KEY"],
-        os.environ["S3_HOST"],
-        os.environ["STATPING_HOST_URL"],
-        os.environ["STATPING_API_TOKEN"],
-    )
-    logging.info(
-        file_name, bucket_name, s3_host, host_url
-    )
+    """Export Statping JSON data to S3."""
+
+    # Load env
+    filename = os.environ["FILE_NAME"]
+    bucketname = os.environ["BUCKET_NAME"]
+    s3_access_key = os.environ["S3_ACCESS_KEY"]
+    s3_secret_key = os.environ["S3_SECRET_KEY"]
+    s3_host = os.environ["S3_HOST"]
+    statping_host_url = os.environ["STATPING_HOST_URL"]
+    statping_api_token = os.environ["STATPING_API_TOKEN"]
+    cmk_description = os.environ["CMK_DESCRIPTION"]
+
+    # Export Statping services JSON
     try:
-        logging.info(host_url)
         response = requests.get(
-            host_url, headers={"Authorization": "Bearer {}".format(token)}
+            statping_host_url,
+            headers={"Authorization": "Bearer {}".format(statping_api_token)},
         )
-        logging.info(response.status_code)
         services_json = response.json()
-        json.dump(services_json, file_name)
+        json.dump(services_json, filename)
     except Exception as e:
         logging.info("{} occured while exporting services.".format(e))
 
-    object_name = os.path.basename(file_name)
+    # Encrypt and Upload to S3
+    cmk_id, _ = create_cmk(cmk_description)
+    encrypt_file(filename, cmk_id)
+    object_name = os.path.basename(filename)
     s3 = boto3.client(
         "s3",
-        aws_access_key_id=access_key,
-        aws_secret_access_key=secret_key,
+        aws_access_key_id=s3_access_key,
+        aws_secret_access_key=s3_secret_key,
         endpoint_url=s3_host,
     )
     try:
-        s3.upload_file(file_name, bucket_name, object_name)
+        s3.upload_file(filename, bucketname, object_name)
         logging.info("Statping services exported and uploaded successfully.")
     except Exception as e:
         logging.info("{} occured while uploading file to S3.".format(e))
+
+    return
 
 
 if __name__ == "__main__":
---
+++ import-services.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+
+import base64
+import json
+import logging
+import os
+
+import boto3
+import requests
+from cryptography.fernet import Fernet
+
+NUM_BYTES_FOR_LEN = 4
+
+
+def decrypt_data_key(data_key_encrypted):
+    """Decrypt an encrypted data key"""
+
+    # Decrypt the data key
+    kms_client = boto3.client("kms")
+    response = kms_client.decrypt(CiphertextBlob=data_key_encrypted)
+
+    # Return plaintext base64-encoded binary data key
+    return base64.b64encode((response["Plaintext"]))
+
+
+def decrypt_file(filename):
+    """Decrypt a file encrypted by encrypt_file()"""
+
+    # Read the encrypted file into memory
+    with open(filename + ".encrypted", "rb") as file:
+        file_contents = file.read()
+
+    # The first NUM_BYTES_FOR_LEN tells us the length of the encrypted data key
+    # Bytes after that represent the encrypted file data
+    data_key_encrypted_len = (
+        int.from_bytes(file_contents[:NUM_BYTES_FOR_LEN], byteorder="big")
+        + NUM_BYTES_FOR_LEN
+    )
+    data_key_encrypted = file_contents[NUM_BYTES_FOR_LEN:data_key_encrypted_len]
+
+    # Decrypt the data key before using it
+    data_key_plaintext = decrypt_data_key(data_key_encrypted)
+    if data_key_plaintext is None:
+        return False
+
+    # Decrypt the rest of the file
+    f = Fernet(data_key_plaintext)
+    file_contents_decrypted = f.decrypt(file_contents[data_key_encrypted_len:])
+
+    # Write the decrypted file contents
+    with open(filename, "wb") as file_decrypted:
+        file_decrypted.write(file_contents_decrypted)
+
+
+def create_services():
+    """Import JSON data to Statping."""
+
+    # Load env
+    filename = os.environ["FILE_NAME"]
+    bucketname = os.environ["BUCKET_NAME"]
+    s3_access_key = os.environ["S3_ACCESS_KEY"]
+    s3_secret_key = os.environ["S3_SECRET_KEY"]
+    s3_host = os.environ["S3_HOST"]
+    statping_host_url = os.environ["STATPING_HOST_URL"]
+    statping_api_token = os.environ["STATPING_API_TOKEN"]
+
+    statping_services = None
+    s3 = boto3.client(
+        "s3",
+        aws_access_key_id=s3_access_key,
+        aws_secret_access_key=s3_secret_key,
+        endpoint_url=s3_host,
+    )
+    try:
+        s3_response = s3.get_object(Bucket=bucketname, Key=filename)
+        statping_services = s3_response["Body"].read().decode("utf-8")
+        json.dump(statping_services, filename)
+    except Exception as e:
+        logging.info("{} occured while fetching S3 bucket.".format(e))
+
+    if statping_services:
+        decrypt_file(filename)
+        with open(filename, "rb") as file_decrypted:
+            obj = json.load(file_decrypted)
+        headers = {"Authorization": "Bearer {}".format(statping_api_token)}
+        try:
+            response = requests.post(statping_host_url, headers=headers, json=obj)
+            if response.status_code == 200:
+                logging.info("Statping service exported and uploaded successfully.")
+            else:
+                logging.info("{}".format(response.json["error"]))
+        except Exception as e:
+            logging.info("{} occured while craeting the statping service.".format(e))
